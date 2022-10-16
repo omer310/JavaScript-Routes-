cf60a845ff0d99cd902f9c410e7e4ba316dcf934
omer310 <50899819+omer310@users.noreply.github.com>
2022-10-16 19:07:35 -0400

    Get and Post Project

---
+++ index.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false})); 
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname,'./Public', 'index.html'));
+});
+
+app.get("/Omar", (req, res) => {
+	res.sendFile(path.join(__dirname,'./Public', 'Omar.html'));
+});
+
+app.post("/comment", (req, res) => {
+    res.send(req.body); 
+});
+
+app.listen(3000, () => console.log("listening on port 3000"));
